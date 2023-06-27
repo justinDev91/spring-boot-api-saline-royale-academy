aca437aaed0dc550b17b3da5157b0347f9c5c784
justinDev91 <justin@sustainsoft.eu>
2023-06-27 11:11:38 +0200

    feat(app): Initialize spring-boot app

---
+++ src/test/java/com/salineacademy/api/SalineAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salineacademy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalineAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
