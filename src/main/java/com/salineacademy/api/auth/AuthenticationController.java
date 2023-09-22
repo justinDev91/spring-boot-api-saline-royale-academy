95ff3d280e2176e7fa2a83b7bd3179be83f3fa74
justinDev91 <justin@sustainsoft.eu>
2023-09-22 13:18:04 +0200

    Hot-fix

--- src/main/java/com/salineacademy/api/auth/AuthenticationController.java
+++ src/main/java/com/salineacademy/api/auth/AuthenticationController.java
@@ -1,44 +1,44 @@
-package com.salineacademy.api.auth;
+// package com.salineacademy.api.auth;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import lombok.RequiredArgsConstructor;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.salineacademy.api.auth.dto.AuthenticationRequestDTO;
-import com.salineacademy.api.auth.dto.AuthenticationResponseDTO;
-import com.salineacademy.api.auth.dto.RegisterRequestDTO;
+// import com.salineacademy.api.auth.dto.AuthenticationRequestDTO;
+// import com.salineacademy.api.auth.dto.AuthenticationResponseDTO;
+// import com.salineacademy.api.auth.dto.RegisterRequestDTO;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-@RestController
-@RequestMapping("/api/v1/auth")
-@RequiredArgsConstructor
-public class AuthenticationController {
+// @RestController
+// @RequestMapping("/api/v1/auth")
+// @RequiredArgsConstructor
+// public class AuthenticationController {
 
-  private final AuthenticationService service;
+//   private final AuthenticationService service;
 
-  @PostMapping("/register")
-  public ResponseEntity<AuthenticationResponseDTO> register(
-      @RequestBody RegisterRequestDTO request) {
-    return ResponseEntity.ok(service.register(request));
-  }
+//   @PostMapping("/register")
+//   public ResponseEntity<AuthenticationResponseDTO> register(
+//       @RequestBody RegisterRequestDTO request) {
+//     return ResponseEntity.ok(service.register(request));
+//   }
 
-  @PostMapping("/authenticate")
-  public ResponseEntity<AuthenticationResponseDTO> authenticate(
-      @RequestBody AuthenticationRequestDTO request) {
-    return ResponseEntity.ok(service.authenticate(request));
-  }
+//   @PostMapping("/authenticate")
+//   public ResponseEntity<AuthenticationResponseDTO> authenticate(
+//       @RequestBody AuthenticationRequestDTO request) {
+//     return ResponseEntity.ok(service.authenticate(request));
+//   }
 
-  @PostMapping("/refresh-token")
-  public void refreshToken(
-      HttpServletRequest request,
-      HttpServletResponse response) throws IOException {
-    service.refreshToken(request, response);
-  }
+//   @PostMapping("/refresh-token")
+//   public void refreshToken(
+//       HttpServletRequest request,
+//       HttpServletResponse response) throws IOException {
+//     service.refreshToken(request, response);
+//   }
 
-}
+// }
