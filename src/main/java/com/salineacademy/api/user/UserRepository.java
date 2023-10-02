35cd9fb374a76db32c97057ee5caa8d0e4555d0f
justinDev91 <justin@sustainsoft.eu>
2023-10-02 21:02:10 +0200

    fix email

--- src/main/java/com/salineacademy/api/user/UserRepository.java
+++ src/main/java/com/salineacademy/api/user/UserRepository.java
@@ -1,12 +1,12 @@
-package com.salineacademy.api.user;
+// package com.salineacademy.api.user;
 
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+// import java.util.Optional;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.salineacademy.api.user.entity.User;
+// import com.salineacademy.api.user.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+// public interface UserRepository extends JpaRepository<User, Integer> {
 
-  Optional<User> findByEmail(String email);
+//   Optional<User> findByEmail(String email);
 
-}
+// }
