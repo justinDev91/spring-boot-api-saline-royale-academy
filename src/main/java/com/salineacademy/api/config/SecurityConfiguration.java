d4ac32c2a737a9658759851bcaf9d585639e2854
justinDev91 <justin@sustainsoft.eu>
2023-10-02 21:01:47 +0200

    fix auth

--- src/main/java/com/salineacademy/api/config/SecurityConfiguration.java
+++ src/main/java/com/salineacademy/api/config/SecurityConfiguration.java
@@ -1,55 +1,55 @@
-// package com.salineacademy.api.config;
+package com.salineacademy.api.config;
 
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.authentication.AuthenticationProvider;
-// import
-// org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.http.SessionCreationPolicy;
-// import org.springframework.security.web.SecurityFilterChain;
-// import
-// org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-// import com.salineacademy.api.config.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import
+org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import
+org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import
+org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import
+org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.salineacademy.api.config.jwt.JwtAuthenticationFilter;
 
-// @Configuration
-// @EnableWebSecurity
-// @RequiredArgsConstructor
-// @EnableMethodSecurity
-// public class SecurityConfiguration {
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfiguration {
 
-// private final JwtAuthenticationFilter jwtAuthFilter;
-// private final AuthenticationProvider authenticationProvider;
+private final JwtAuthenticationFilter jwtAuthFilter;
+private final AuthenticationProvider authenticationProvider;
 
-// @Bean
-// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-// Exception {
-// http.authorizeHttpRequests(authorize -> authorize
-// .requestMatchers(
-// "/",
-// "/api/v1/auth/**",
-// "/v2/api-docs",
-// "/v3/api-docs",
-// "/v3/api-docs/**",
-// "/swagger-resources",
-// "/swagger-resources/**",
-// "/configuration/ui",
-// "/configuration/security",
-// "/swagger-ui/**",
-// "/webjars/**",
-// "/swagger-ui.html"
-// ).permitAll()
-// .anyRequest().authenticated()
-// )
-// .sessionManagement(session -> session
-// .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-// )
-// .authenticationProvider(authenticationProvider)
-// .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-// return http.build();
-// }
-// }
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+Exception {
+http.authorizeHttpRequests(authorize -> authorize
+.requestMatchers(
+"/",
+"/api/v1/auth/**",
+"/v2/api-docs",
+"/v3/api-docs",
+"/v3/api-docs/**",
+"/swagger-resources",
+"/swagger-resources/**",
+"/configuration/ui",
+"/configuration/security",
+"/swagger-ui/**",
+"/webjars/**",
+"/swagger-ui.html"
+).permitAll()
+.anyRequest().authenticated()
+)
+.sessionManagement(session -> session
+.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+)
+.authenticationProvider(authenticationProvider)
+.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+return http.build();
+}
+}
