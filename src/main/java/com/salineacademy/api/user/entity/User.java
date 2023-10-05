ba5f8832ddfc8879f0dff6609f9a4f3f55b6897e
justinDev91 <justin@sustainsoft.eu>
2023-10-05 19:57:03 +0200

    user endpoint

--- src/main/java/com/salineacademy/api/user/entity/User.java
+++ src/main/java/com/salineacademy/api/user/entity/User.java
@@ -36,44 +36,44 @@ public class User {
   private String email;
   private String password;
 
-  // @Enumerated(EnumType.STRING)
-  // private Role role;
+  @Enumerated(EnumType.STRING)
+  private Role role;
 
-  // @OneToMany(mappedBy = "user")
-  // private List<Token> tokens;
+  @OneToMany(mappedBy = "user")
+  private List<Token> tokens;
 
-  // @Override
-  // public Collection<? extends GrantedAuthority> getAuthorities() {
-  //   return role.getAuthorities();
-  // }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return role.getAuthorities();
+  }
 
-  // @Override
-  // public String getPassword() {
-  //   return password;
-  // }
+  @Override
+  public String getPassword() {
+    return password;
+  }
 
-  // @Override
-  // public String getUsername() {
-  //   return email;
-  // }
+  @Override
+  public String getUsername() {
+    return email;
+  }
 
-  // @Override
-  // public boolean isAccountNonExpired() {
-  //   return true;
-  // }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-  // @Override
-  // public boolean isAccountNonLocked() {
-  //   return true;
-  // }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-  // @Override
-  // public boolean isCredentialsNonExpired() {
-  //   return true;
-  // }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-  // @Override
-  // public boolean isEnabled() {
-  //   return true;
-  // }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
